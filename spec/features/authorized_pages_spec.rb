3d1648d0f2babee37f62a738e3862092d52dea18
Starlynne <lewis.start0905@gmail.com>
2017-07-25 13:29:14 -0700

    Add integration spec for redirect from admin if not authorized

--- spec/features/authorized_pages_spec.rb
+++ spec/features/authorized_pages_spec.rb
@@ -15,5 +15,14 @@ describe "User will have admin access if signed in as admin user" do
     visit 'home#index'
     expect(page).to have_content 'Admin'
   end
-  
+end
+
+describe "User will not have admin access not an admin" do
+  it "redirects to login in if non admin tries to access dashboard" do
+    user = FactoryGirl.create(:user)
+    login_as(user)
+    visit 'home#index'
+    visit 'http://localhost:3000/admin'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
 end
